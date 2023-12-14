b728504d75709e7c2b834e39a2854c358c073099
Sam Miller <sammiller@sams-mbp.mynetworksettings.com>
2023-12-13 20:20:38 -0500

    fixed tests

--- src/world_peace/stats.py
+++ src/world_peace/stats.py
@@ -1,6 +1,7 @@
 from decimal import Decimal
-from world_peace.model import Portfolio
+from world_peace.model import Portfolio, DrawDown
 from statistics import mean, stdev
+from pydantic import BaseModel
 
 
 class PortfolioStatistics:
@@ -8,26 +9,49 @@ class PortfolioStatistics:
 
     risk_free_rate: Decimal = Decimal(0.0)
     portfolio: Portfolio
+    hp: dict[int, Portfolio]
 
-    def __init__(self, portfolio: Portfolio):
+    def __init__(self, portfolio: Portfolio, hp: dict):
         self.portfolio = portfolio
+        self.historical_portfolios = hp
 
     # TODO
     def compound_annual_growth_rate(self):
-        average_returns = mean([a.rate_of_return for a in self.portfolio.positions])
-        return average_returns
+        current_value = sum(a.market_value for a in self.portfolio.positions)
+        starting_value = self.historical_portfolios[0].total_market_value
+        print(current_value / starting_value, self.historical_portfolios)
+        print("time_idx", self.portfolio.time_idx)
+        cagr = ((current_value / starting_value) ** (Decimal(1) / Decimal(self.portfolio.time_idx)) - 1)
+        return cagr
 
     def sharp_ratio(self):
-        portfolio_returns = [a.rate_of_return for a in self.portfolio.positions]
+        historical_returns = []
+        for k, v in self.historical_portfolios.items():
+            for p in v.positions:
+                historical_returns.append(p.rate_of_return)
+
+        portfolio_returns = [a.rate_of_return for a in self.portfolio.positions] + historical_returns
         portfolio_std = stdev(portfolio_returns)
         sharp_ratio = (self.compound_annual_growth_rate() - self.risk_free_rate) / portfolio_std
         return round(sharp_ratio, 2)
 
-    def calmar_ratio(self, *args, **kwargs):
-        raise NotImplementedError()
+    def calmar_ratio(self, *args, **kwargs) -> float:
+        return self.compound_annual_growth_rate() / self.drawdown().percent
 
-    def maximum_drawdown(self, *args, **kwargs):
-        raise NotImplementedError()
-
-    def maximum_drawdown_duration(self, *args, **kwargs):
-        raise NotImplementedError()
+    def drawdown(self, *args, **kwargs) -> DrawDown:
+        historical_portfolios: list[Portfolio] = [v for v in self.historical_portfolios.values()]
+        best_day: Portfolio = max(historical_portfolios, key=lambda p: p.total_market_value)
+        worst_day: Portfolio = min(historical_portfolios[best_day.time_idx:], key=lambda p: p.total_market_value)
+        draw_down_percent = (best_day.total_market_value - worst_day.total_market_value) / best_day.total_market_value
+        draw_down_duration = abs(worst_day.time_idx - best_day.time_idx)
+        # 1. Find Max portfolio, find min portfolio
+        # 2.  MDD = (Peak Value - Trough Value) / Peak Value
+        # 3. count number of values between peak value and trough value
+        print(f"best {best_day} \n worst {worst_day}"
+              f""
+              f"pct: {draw_down_percent} dur: {draw_down_duration}")
+        return DrawDown(
+            time_idx=worst_day.time_idx,
+            percent=draw_down_percent,
+            duration=draw_down_duration
+        )
--- tests/test_statistics.py
+++ tests/test_statistics.py
@@ -8,19 +8,136 @@ from decimal import Decimal
 def profitable_position() -> Position:
     return Position(
         asset="AAPL",
-        rate_of_return=2.0
+        rate_of_return=2.0,
+        start_idx=1,
+        market_value=200.0,
+        notional=1,
     )
 
 
 @fixture
-def single_asset_portfolio(profitable_position) -> Portfolio:
+def single_asset_portfolio(profitable_position: Position) -> Portfolio:
     return Portfolio(
-        positions=[profitable_position]
+        positions=[profitable_position],
+        time_idx=2,
     )
 
 
-def test_single_asset_portfolio(single_asset_portfolio) -> None:
-    portfolio_statistics = PortfolioStatistics(portfolio=single_asset_portfolio)
+@fixture
+def historical_single_asset_portfolios(profitable_position) -> dict[int, Portfolio]:
+    cash_start = Position(
+        **{
+            "rate_of_return": 0.0,
+            "start_idx": 0,
+            "market_value": 100.0,
+            "notional": 100,
+            "time_index": 0,
+            "asset": "CASH"
+        }
+    )
+    first_position = Position(
+        **{
+            "rate_of_return": 0.0,
+            "start_idx": 1,
+            "market_value": 100.0,
+            "notional": 1,
+            "time_index": 1,
+            "asset": "AAPL"
+        }
+    )
 
-    assert portfolio_statistics.compound_annual_growth_rate() == 1.0
-    assert portfolio_statistics.sharp_ratio() == Decimal("0.71")
+    first_cash_portfolio = Portfolio(positions=[cash_start], time_idx=0)
+    first_traded_portfolio = Portfolio(positions=[first_position], time_idx=1)
+
+    return {
+        0: first_cash_portfolio,
+        1: first_traded_portfolio,
+    }
+
+
+@fixture
+def historical_portfolio_with_drawdown(historical_single_asset_portfolios) -> dict[int, Portfolio]:
+    first_position_with_return = Position(
+        **{
+            "rate_of_return": 1.0,
+            "start_idx": 1,
+            "market_value": 200.0,
+            "notional": 1,
+            "time_index": 2,
+            "asset": "AAPL"
+        }
+    )
+    first_position_with_drawdown = Position(
+        **{
+            "rate_of_return": 1.0,
+            "start_idx": 1,
+            "market_value": 150.0,
+            "notional": 1,
+            "time_index": 3,
+            "asset": "AAPL"
+        }
+    )
+
+    first_position_increased = Position(
+        **{
+            "rate_of_return": 1.0,
+            "start_idx": 1,
+            "market_value": 170.0,
+            "notional": 1,
+            "time_index": 4,
+            "asset": "AAPL"
+        }
+    )
+
+    first_position_with_return = Portfolio(positions=[first_position_with_return],
+                                           time_idx=2)
+
+    first_position_with_drawdown = Portfolio(positions=[first_position_with_drawdown],
+                                             time_idx=3)
+    first_position_increased = Portfolio(positions=[first_position_increased],
+                                             time_idx=4)
+
+    return {
+        **historical_single_asset_portfolios,
+        2: first_position_with_return,
+        3: first_position_with_drawdown,
+        4: first_position_increased
+    }
+
+
+@fixture
+def single_asset_statistics(single_asset_portfolio, historical_single_asset_portfolios) -> PortfolioStatistics:
+    portfolio_statistics = PortfolioStatistics(portfolio=single_asset_portfolio,
+                                               hp=historical_single_asset_portfolios)
+    return portfolio_statistics
+
+
+@fixture
+def single_asset_statistics_with_drawdown(single_asset_portfolio,
+                                          historical_portfolio_with_drawdown) -> PortfolioStatistics:
+    stats = PortfolioStatistics(portfolio=single_asset_portfolio,
+                                               hp=historical_portfolio_with_drawdown)
+    return stats
+
+
+def test_sharp_ratio__single_asset_portfolio(single_asset_statistics) -> None:
+    assert round(single_asset_statistics.compound_annual_growth_rate(), 2) == Decimal(".41")
+    # TODO check sharp ratio math
+    assert single_asset_statistics.sharp_ratio() == Decimal("0.36")
+
+
+def test_maximum_drawdown__single_asset_portfolio(
+        single_asset_statistics_with_drawdown: PortfolioStatistics
+) -> None:
+    drawdown = single_asset_statistics_with_drawdown.drawdown()
+    assert round(float(drawdown.percent), 2) == .25
+    assert drawdown.duration == 1
+    assert drawdown.time_idx == 3
+
+
+def test_calmar_ratio__single_asset_portfolio(
+        single_asset_statistics_with_drawdown: PortfolioStatistics
+) -> None:
+    drawdown = single_asset_statistics_with_drawdown.drawdown()
+    cagr = single_asset_statistics_with_drawdown.compound_annual_growth_rate()
+    assert cagr / drawdown.percent == single_asset_statistics_with_drawdown.calmar_ratio()
